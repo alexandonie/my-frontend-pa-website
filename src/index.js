feb1c17ce21b36a2b71bdb0b97ee2516bccb15a8
Alex Andonie <andonie.alex@gmail.com>
2019-08-13 17:18:35 +0300

    removed the billboard interaction/animation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 import './styles/main.scss';
-
-import PeekAnimation from './scripts/PeekAnimation';
-
-// peekAnimation(document.querySelector('#peekAnimation'));
-
-const peekAnimation = new PeekAnimation(document.querySelector('#peekAnimation'));
-peekAnimation.init();
-
-console.log('hello world');
-
-// bla();
