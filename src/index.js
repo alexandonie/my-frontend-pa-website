fc705e228e856ce5472d7f225201e55a3fa82710
Alex Andonie <andonie.alex@gmail.com>
2019-08-14 15:15:10 +0300

    fixed ie11 js issue

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import './styles/main.scss';
 
+/**
+ * Babel polyfill for IE11
+ * https://babeljs.io/docs/en/babel-polyfill/
+*/
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 import RandomBadgePeek from './scripts/RandomBadgePeek';
 
 const randomBadgePeek = new RandomBadgePeek(document.querySelector('#randomBadgePeek'));
